d570c8fef137f49f3a4e2d55ee9381356f6a758b
Daniel Melichar <danielmelichar0905@gmail.com>
2014-03-19 18:16:59 +0100

    Erster Funktionierender Code
    To-Do:
    	- Kommentare
    	- Klassenkomentare
    	- JUnit

--- Decypher/src/melichar/cipher/MonoalphabeticCipher.java
+++ Decypher/src/melichar/cipher/MonoalphabeticCipher.java
@@ -4,9 +4,9 @@ public class MonoalphabeticCipher implements Cipher {
 
 	private String secretAlphabet;
 	
-	public MonoalphabeticCipher()
+	public MonoalphabeticCipher(String secretAlphabet)
 	{
-		
+		this.secretAlphabet = secretAlphabet;
 	}
 	
 	public String getSecretAlphabet()
@@ -21,14 +21,49 @@ public class MonoalphabeticCipher implements Cipher {
 
 	@Override
 	public String encrypt(String text) {
-		// TODO Auto-generated method stub
-		return null;
+        char[] bsarray = secretAlphabet.toCharArray();
+        String returntext = "";
+        
+        for (int i = 0; i < text.length(); i++) {
+                char buchstabe = text.charAt(i);
+                if (buchstabe >= 'a' && buchstabe <= 'z') {
+                        returntext = returntext + bsarray[buchstabe - 97];
+                } else if (buchstabe >= 'A' && buchstabe <= 'Z') {
+                        returntext = returntext + bsarray[buchstabe - 65];
+                } else {
+                        System.err.println("Fehler: Ein Sonderzeichen oder eine Zahl war im Text.");
+                        System.exit(0);
+                }
+        }
+        returntext = returntext.toUpperCase();
+        return returntext;
 	}
 
 	@Override
 	public String deencrypt(String text) {
-		// TODO Auto-generated method stub
-		return null;
+        char[] bsarray = secretAlphabet.toCharArray();
+        String returntext = "";
+        for (int i = 0; i < text.length(); i++) {
+                char buchstabe = text.charAt(i);
+                if (buchstabe >= 'a' && buchstabe <= 'z') {
+                        for (int a = 0; a < bsarray.length; a++) {
+                                if (((char) buchstabe - 32) == bsarray[a]) {
+                                        returntext = returntext + (char) ('a' - 32 + a);
+                                }
+                        }
+                } else if (buchstabe >= 'A' && buchstabe <= 'Z') {
+                        for (int a = 0; a < bsarray.length; a++) {
+                                if (buchstabe == bsarray[a]) {
+                                        returntext = returntext + (char) ('A' + a);
+                                }
+                        }
+                } else {
+                        System.err.println("Fehler: Ein Sonderzeichen oder eine Zahl war im Text.");
+                        System.exit(0);
+                }
+        }
+        returntext = returntext.toLowerCase();
+        return returntext;
 	}
 	
 }
